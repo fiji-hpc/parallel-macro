dba51f8cd7b9de6de0cff985a4ffad310f19ab81
Dimitrios Stefanos Velissariou <velissarious@gmail.com>
2020-05-06 12:10:13 +0200

    add an alternative more parallelization
    The new script follows an approach that has a more evenly distributed workload for the compute nodes and achieves better results with some node combinations .

--- src/main/resources/ExampleScripts/Bruno/runAllExperiments.sh
+++ src/main/resources/ExampleScripts/Bruno/runAllExperiments.sh
@@ -12,13 +12,17 @@ budget=OPEN-17-47
 # Loop through all possible compute node numbers:
 for ((compute_nodes_number=1; compute_nodes_number<=$views; compute_nodes_number++))
 do
-        echo "Compute node number $compute_nodes_number"
+	echo "Compute node number $compute_nodes_number"
 
-        # Repeat ten times to get accurate results:
-        for((repetition=1; repetition<=$repetitions; repetition++))
-        do
-                echo "Repetition number $repetition"
-                # Submit the job:
-				qsub -A $budget -q qprod -o $HOME/.scijava-parallel/ -e $HOME/.scijava-parallel/ -l select=$compute_nodes_number:ncpus=24:mpiprocs=1:ompthreads=24 -l walltime=00:20:00 ./runCommand.sh
-        done
+	# Repeat ten times to get accurate results:
+	for((repetition=1; repetition<=$repetitions; repetition++))
+	do
+		echo "Repetition number: $repetition"
+		# Submit the job:
+		qsub -A $budget -q qprod -o $HOME/.scijava-parallel/ -e $HOME/.scijava-parallel/ -l select=$compute_nodes_number:ncpus=24:mpiprocs=1:ompthreads=24 -l walltime=00:20:00 ./runCommand.sh
+		echo "Will wait for 25 minutes before submitting another job."
+
+		# Wait 25 minutes to avoid submitting too many jobs at once.
+		sleep 25m
+	done
 done 
